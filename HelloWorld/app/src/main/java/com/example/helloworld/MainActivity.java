f37280867d1d0d3fcfcac28008183d2635eeffcb
Neha P <68220580+neha277@users.noreply.github.com>
2022-02-05 01:35:02 -0500

    Codepath prework project

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ( (TextView)findViewById(R.id.text1)).setTextColor(getResources().getColor(R.color.teal_200));
+            }
+        });
+    }
+}
